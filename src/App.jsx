fc6c402a87c0372d1257565d4fe51200613733f9
Nguyenduyquang123 <nguyenduyquang240403@gmail.com>
2024-07-19 10:28:55 +0700

    code

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import { pink } from '@mui/material/colors'
+import HomeIcon from '@mui/icons-material/Home'
+import Button from '@mui/material/Button'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>Nguyễn Duy Quang</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <br />
+      <AccessAlarmIcon/>
+      <ThreeDRotation/>
+      <br />
+      <HomeIcon />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+      <HomeIcon sx={{ color: pink[500] }} />
+    </>
+  )
+}
+
+export default App
